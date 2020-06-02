bc10af87fe32d2b58d143ec682ffd92f199d9ebc
Shahid <shahidcseku@gmail.com>
2020-06-02 18:11:32 +0500

    con hard coded

--- includes/connect.php
+++ includes/connect.php
@@ -5,6 +5,11 @@
 	$password = getenv('db_password');
 	$db = getenv('db_name');
 
+	$host = "ec2-34-202-88-122.compute-1.amazonaws.com";
+	$user = "pkquzhrohjzlvf";
+	$password = "d7a5e0641db7b8c6d39d461135e90df8f4db638c44656a0e6e12748b9acf0cff";
+	$db = "d2l7tudouu3orq";
+
 	// Local 
 
 	// $host = "localhost";
