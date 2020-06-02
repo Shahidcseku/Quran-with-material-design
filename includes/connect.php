5e23f878bb56cba15c9d39ba8ac88f208509cf59
Mohamed Shahid <shahid1506@cseku.ac.bd>
2020-06-02 17:21:00 +0500

    read environment variables

--- includes/connect.php
+++ includes/connect.php
@@ -1,8 +1,8 @@
 <?php
-	$host = "localhost";
-	$user = "ss";
-	$password = "ss";
-	$db = "quran";
+	$host = getenv('db_host');
+	$user = getenv('db_user');
+	$password = getenv('db_password');
+	$db = getenv('db_name');
 	$con = mysqli_connect($host,$user,$password,$db) or die ("Couldn't connnect".mysql_error());
 	$con->set_charset("utf8")
 ?>
