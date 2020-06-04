f1dba3943b33aaf8575116625f5b8e344ba12909
Shahid <shahidcseku@gmail.com>
2020-06-04 06:31:36 +0500

    mysqli connection

--- dal/dal.quran.php
+++ dal/dal.quran.php
@@ -1,6 +1,4 @@
 <?php 
-include_once($_SERVER['DOCUMENT_ROOT'].'/Quran/includes/utility.php');
-
 class DALQuran
 {
 	
--- includes/connect.php
+++ includes/connect.php
@@ -7,12 +7,12 @@
 	
 	// Local 
 
-	$host = "localhost";
-	$user = "root";
-	$password = "";
-	$db = "quran";
+	// $host = "localhost";
+	// $user = "root";
+	// $password = "";
+	// $db = "quran";
 
-	$con = mysqli_connect($host,$user,$password,$db) or die ("Couldn't connnect".mysql_error());
+	$con = mysqli_connect($host,$user,$password,$db) or die ("Couldn't connnect".mysqli_error());
 	$connect = new mysqli($host,$user,$password,$db);
 	$con->set_charset("utf8")
 ?>
